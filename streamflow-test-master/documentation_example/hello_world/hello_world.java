ba50b8df960ffd1c56a937c893203cae09d0ca9f
Duong37 <duong.vu.hai@student.uva.nl>
2025-08-02 17:14:11 +0200

    remove orchestr server, streamflow-test-master

--- streamflow-test-master/documentation_example/hello_world/hello_world.java
+++
@@ -1,8 +0,0 @@
-// Hello_World.java
-public class Hello_World {
-
-    public static void main(String[] args) {
-        // Print "Hello, World!" naar de console
-        System.out.println("Hello, World!");
-    }
-}
